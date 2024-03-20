da7720037f015e49ec8f5f5fd90aec30d7c868f5
Fakhar Mumtaz <101429894+fakharm@users.noreply.github.com>
2024-03-20 01:37:22 -0400

    First Springboot project for profile demo

---
+++ SpringbootProfileDemo/src/main/java/com/gen/profile/demo/SpringbootProfileDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gen.profile.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProfileDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProfileDemoApplication.class, args);
+	}
+
+}
---
+++ SpringbootProfileDemo/src/test/java/com/gen/profile/demo/SpringbootProfileDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gen.profile.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProfileDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
