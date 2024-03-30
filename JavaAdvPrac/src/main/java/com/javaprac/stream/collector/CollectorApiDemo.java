4d835d74bfd7a0e053245bcbc84f6a52fc22cb0c
Fakhar Mumtaz <101429894+fakharm@users.noreply.github.com>
2024-03-30 02:57:58 -0400

    Streams and Optional example1

--- JavaAdvPrac/src/main/java/com/javaprac/stream/collector/CollectorApiDemo.java
+++ JavaAdvPrac/src/main/java/com/javaprac/stream/collector/CollectorApiDemo.java
@@ -2,6 +2,7 @@ package com.javaprac.stream.collector;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class CollectorApiDemo {
     public static void main(String[] args) {
@@ -10,6 +11,21 @@ public class CollectorApiDemo {
         List<Integer> listOfInt = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
         Optional<Integer> first = listOfInt.stream().filter(e -> e % 2 == 0).findFirst();
         //first.isPresent()
+        Stream<Integer> integerStream = listOfInt.stream().filter(e -> e % 2 == 0);
 
+        var intgerStrm = listOfInt.stream().filter(e -> e % 2 == 0);
+        intgerStrm.forEach(System.out::println);
+
+        var count = listOfInt.stream().filter(CollectorApiDemo::isEven).count();
+        System.out.println("count=" + count);
+
+    }
+
+     private static boolean isEven(int e){
+        return e % 2 == 0;
+     }
+
+    private static Integer doubleIt(int e){
+        return 3 * 2;
     }
 }
